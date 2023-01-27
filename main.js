cf03de505743acc21d1490bda2a5cf6d56ceda29
allenaw12 <102706128+allenaw12@users.noreply.github.com>
2023-01-27 15:36:16 -0600

    basic functionality works, addition subtraction division multiplication with equals works, not negative numbers

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 ////variables
-    //display element
-    let displayCurrent = document.querySelector('span').innerText
+    //display element from dom
+    let displayCurrent = document.querySelector('span')
     //event.target gets the specific target value
     let value = ''
     //num1 value, hold if operator is clicked
@@ -29,80 +29,84 @@ function clicky(event){
     //first number will always be what's currently in display window
     //second number will come after operator button press, need way to store first number
     //when another operator or equal sign is pressed, function neeeds to run math operation
-    function addition(num1, num2){
-        console.log('adding'+num1 + +num2)
-        return +num1 + +num2
+    function addition(num){
+        let nums = num.split('+')
+        console.log('adding'+nums[0] , +nums[1])
+        return +nums[0] + +nums[1]
     }
-    function subtraction(num1, num2){
-        console.log('subbing'+num1 - +num2)
-        return +num1 - +num2
+    function subtraction(num){
+        let nums = num.split('−')
+        console.log('subbing'+nums[0] , +nums[1])
+        return +nums[0] - +nums[1]
     }
-    function division(num1, num2){
-        console.log('dividing'+num1 / +num2)
-        return +num1 / +num2
+    function division(num){
+        let nums = num.split('/')
+        console.log('dividing'+nums[0] , +nums[1])
+        return +nums[0] / +nums[1]
     }
-    function multiplication(num1, num2){
-        console.log('multing'+num1 * +num2)
-        return +num1 * +num2
+    function multiplication(num){
+        let nums = num.split('x')
+        console.log('multing' +nums[0] , +nums[1])
+        return +nums[0] * +nums[1]
     }
-
+////if statements for button presses filter
     if(value == Number(value) || value === '.'){
         console.log('val is num')
         if(isNaN(Number(concatNums))){
             console.log('concat is nan')
             concatNums += value
             display = concatNums.split(operation)[1]
-            document.querySelector('span').innerText = display
+            displayCurrent.innerText = display
         }else{
             concatNums += value
-            display += value
-            document.querySelector('span').innerText = display
+            display = concatNums
+            displayCurrent.innerText = display
         }
     }else{
-        if(isNaN(Number(concatNums))){
-            console.log('concat is nan')
-            concatNums += value
-            display = concatNums.split(operation)[1]
-            document.querySelector('span').innerText = display
-        }else{
-        if(value === ('+')){
-            if(concatNums.includes('+')){
-                let nums = concatNums.split('+')
-                display = addition(nums[0], nums[1])
-                console.log(display)
-                operation = '+'
-                concatNums = display.toString() + operation
-                document.querySelector('span').innerText= display
-                console.log(concatNums)
-            }
-            else{
+        if(value === '+'){
+            // if(concatNums.includes('+')){
+            //     let nums = concatNums.split('+')
+            //     display = addition(nums[0], nums[1])
+            //     console.log(display)
+            //     operation = '+'
+            //     concatNums = display.toString() + operation
+            //     document.querySelector('span').innerText= display
+            //     console.log(concatNums)
+            // }
+            // else{
                 operation = value
                 concatNums +=  operation
-                display = ''
-                document.querySelector('span').innerText = display
-                console.log('operator')
-            }
+                display = concatNums.split(operation)[0]
+                displayCurrent.innerText = display
+                console.log('add opp')
+            // }
+        }else if(value === '−'){
+            operation = value
+            concatNums +=  operation
+            display = concatNums.split(operation)[0]
+            displayCurrent.innerText = display
+            console.log('minus opp')
+        }else if(value === 'x'){
+            operation = value
+            concatNums +=  operation
+            display = concatNums.split(operation)[0]
+            displayCurrent.innerText = display
+            console.log('multiply opp')
+        }else if(value === '/'){
+            operation = value
+            concatNums +=  operation
+            display = concatNums.split(operation)[0]
+            displayCurrent.innerText = display
+            console.log('divide opp')
+        }else if(value === '='){
+            operation = ''
+            console.log('problem' ,concatNums)
+            display = concatNums.includes('+')?addition(concatNums):concatNums.includes('/')?division(concatNums):concatNums.includes('x')?multiplication(concatNums):concatNums.includes('−')?subtraction(concatNums):display
+            concatNums = ''
+            displayCurrent.innerText = display
+            console.log('equals')
         }
-        else if(value === '−'){
-            console.log('minus')
-            if(concatNums.includes('−')){
-                let nums = concatNums.split('−')
-                display = subtraction(nums[0], nums[1])
-                console.log(display)
-                operation = '−'
-                concatNums = display.toString() + operation
-                document.querySelector('span').innerText= display
-                console.log(concatNums)
-            }
-            else{
-                operation = value
-                concatNums +=  operation
-                display = ''
-                document.querySelector('span').innerText = display
-                console.log('operator')
-            }
-        }
-    }}
+    }
     console.log(`Button clicked:${value}, Display${display}, PrevNum: ${concatNums}, Operator:${operation}`)
 }
 
